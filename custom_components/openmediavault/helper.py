9a7c5675cdb8f3f087417820c8155391808b95eb
Tomaae <23486452+tomaae@users.noreply.github.com>
2022-03-21 08:41:59 +0100

    added helper.py

---
+++ custom_components/openmediavault/helper.py
@@ -0,0 +1,29 @@
+"""Helper functions for OMV."""
+
+
+# ---------------------------
+#   format_attribute
+# ---------------------------
+def format_attribute(attr):
+    res = attr.replace("-", " ")
+    res = res.capitalize()
+    res = res.replace(" ip ", " IP ")
+    res = res.replace(" mac ", " MAC ")
+    res = res.replace(" mtu", " MTU")
+    res = res.replace("Sfp", "SFP")
+    res = res.replace("Poe", "POE")
+    res = res.replace(" tx", " TX")
+    res = res.replace(" rx", " RX")
+    return res
+
+
+# ---------------------------
+#   format_value
+# ---------------------------
+def format_value(res):
+    res = res.replace("dhcp", "DHCP")
+    res = res.replace("dns", "DNS")
+    res = res.replace("capsman", "CAPsMAN")
+    res = res.replace("wireless", "Wireless")
+    res = res.replace("restored", "Restored")
+    return res
